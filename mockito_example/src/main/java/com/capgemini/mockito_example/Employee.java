36691d5eb18723dac0430f81cc15429e0a186d1c
Pumang Kirange <kirangespumang03@gmail.com>
2025-05-12 11:51:48 +0530

    Second Change

--- mockito_example/src/main/java/com/capgemini/mockito_example/Employee.java
+++ mockito_example/src/main/java/com/capgemini/mockito_example/Employee.java
@@ -18,7 +18,7 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long code;
-
+	//changes made in entity
 	private String name;
 	private double basic;
 
